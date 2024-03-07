0189576dac48525ddaecb85999db4658336dddd4
radeonvii <kjx01771103@cainiao.com>
2024-03-07 17:15:02 +0800

    feat: 样式优化

--- src/layout/default.vue
+++ src/layout/default.vue
@@ -3,10 +3,14 @@ import SideBar from '@/components/side-bar/index.vue'
 </script>
 
 <template>
-  <n-layout has-sider>
-    <SideBar />
-    <n-layout-content>
-      <router-view />
-    </n-layout-content>
-  </n-layout>
+  <main class="flex h-screen overflow-hidden">
+    <n-layout has-sider class="flex w-full">
+      <SideBar class="min-h-screen" />
+      <section class="p-4">
+        <n-layout-content>
+          <router-view />
+        </n-layout-content>
+      </section>
+    </n-layout>
+  </main>
 </template>
