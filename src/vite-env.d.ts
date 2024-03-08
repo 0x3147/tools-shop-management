5e14fdc90ff0cb2342f378dd4253cae567024eba
radeonvii <radeonk@foxmail.com>
2024-03-08 19:57:07 +0800

    feat: init

--- src/vite-env.d.ts
+++ src/vite-env.d.ts
@@ -11,6 +11,6 @@ declare module '*.vue' {
 }
 
 interface ImportMetaEnv {
-  readonly VITE_NODE_ENV:string;//定义提示信息 数据是只读的无法被修改
+  readonly VITE_NODE_ENV: string //定义提示信息 数据是只读的无法被修改
   //多个变量定义多个...
 }
